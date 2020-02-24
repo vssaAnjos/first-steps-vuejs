9c7842a4071c162e20b358ea325886b15f71fc3b
vssaAnjos <anjnessa9@gmail.com>
2020-02-24 17:07:52 -0300

    add v-model e métodos

--- scripts.js
+++ scripts.js
@@ -1,12 +1,23 @@
 const app= new Vue({
     el:'#app',
-    data:{
+    data:
+    {
         titulo: 'Ola Mundo com Vuejs',
         animais:['macaco','cachorro'],
         frutas:[
             {nome:'maça',quantidade:10},
             {nome:'banana',quantidade:20},
             {nome:'limão',quantidade:0}
-        ]
+        ],
+        novaFruta: ''
+    },
+    methods:
+    {
+        agruparFruta () {
+            this.frutas.push({
+                nome: this.novaFruta, quantidade:0
+            })
+            console.log('clickou')
+        }
     }
 })
